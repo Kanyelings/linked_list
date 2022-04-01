6b966c3c4bda355493e2995bd6dba688a45d8656
Mofiro Jean <mofirojean@gmail.com>
2022-04-01 17:18:35 +0100

    Just the basic java code

---
+++ src/com/mofirojean/Main.java
@@ -0,0 +1,11 @@
+package com.mofirojean;
+// This Java Program implements a linked list using a Java Interface
+interface linkedListSnip{
+    // write code here
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
