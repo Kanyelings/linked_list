3578b096bc32bf24742966312a4169f78d355137
Elroy Kanye <elroykanye@gmail.com>
2022-04-03 23:36:30 +0100

    Revert "created interface for the linked list."

--- src/ILinkedList.java
+++
@@ -1,9 +0,0 @@
-public interface ILinkedList {
-    public int size();
-    public boolean isEmpty();
-    public String first();
-    public String last();
-    public void addFirst();
-    public void addLast();
-    public String remove();
-}
