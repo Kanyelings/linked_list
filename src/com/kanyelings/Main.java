93010d0923a85c892d525d505f0ecac7b0a284b2
Elroy Kanye <elroykanye@gmail.com>
2022-04-03 20:30:29 +0100

    moved from mofiro to kanyelings

---
+++ src/com/kanyelings/Main.java
@@ -0,0 +1,12 @@
+package com.kanyelings;
+
+/**
+ * Main class
+ */
+public class Main {
+    // first create an object of the class before testing
+
+    public static void main(String[] args) {
+
+    }
+}
