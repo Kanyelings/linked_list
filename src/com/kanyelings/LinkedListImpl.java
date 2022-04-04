70d3823199ad7d389ff763469f0f61dba807f9cb
Dezzy237 <desmondneba134@gmail.com>
2022-04-04 14:32:49 +0100

    fixes #6 created a class called LinkedListImpl that implements the interface created

---
+++ src/com/kanyelings/LinkedListImpl.java
@@ -0,0 +1,40 @@
+package com.kanyelings;
+
+public class LinkedListImpl implements ILinkedList{
+    @Override
+    // access methods
+    public int size() { // returns the number of elements in the list
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {   // returns tre if the list is empty, and false if otherwise
+        return false;
+    }
+
+    @Override
+    public String first() {    // returns the first element in the list
+        return null;
+    }
+
+    @Override
+    public String last() {   // returns the last element in the list
+        return null;
+    }
+
+    // update methods
+    @Override
+    public void addFirst() {  //adds a new element to the front of the list
+
+    }
+
+    @Override
+    public void addLast() {  // adds a new element to the end of the list
+
+    }
+
+    @Override
+    public String remove() {  // removes and returns the first element of the list
+        return null;
+    }
+}
